193da7839c695706ba1109f912c455ebc8c55078
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-12-14 23:00:16 +0530

    Application id changed to com.simzmusic.simzacademy

--- simz_academy/android/app/src/main/kotlin/com/example/simz_academy/MainActivity.kt
+++ simz_academy/android/app/src/main/kotlin/com/example/simz_academy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.simz_academy
+package com.simzmusic.simzacademy
 
 import io.flutter.embedding.android.FlutterActivity
 
--- simz_academy/windows/flutter/generated_plugins.cmake
+++ simz_academy/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,8 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   connectivity_plus
+  flutter_inappwebview_windows
+  printing
   url_launcher_windows
 )
 
