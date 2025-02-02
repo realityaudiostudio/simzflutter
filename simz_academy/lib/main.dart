4d74b93dc24ab5274ea78b71b9866e34f99b5983
christopherjneelankavil <christopherneelankavil@gmail.com>
2025-02-02 22:43:08 +0530

    Updated login methods

--- simz_academy/lib/main.dart
+++ simz_academy/lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simz_academy/controllers/constants/supabase_functions.dart';
-//import 'package:simz_academy/views/screens/login_screen.dart';
 import 'package:simz_academy/views/screens/splash_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-      url: supaBaseUrl,
-      anonKey:supaAnonKey
+    url: supaBaseUrl,
+    anonKey: supaAnonKey,
   );
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,10 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
-      title : 'Simz Academy',
+      title: 'Simz Academy',
     );
   }
 }
